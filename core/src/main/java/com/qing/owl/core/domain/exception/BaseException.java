2e2d395e662e93042f977d4e579ff125c1720ea1
WaylonCoffee <33085998+WaylonCoffee@users.noreply.github.com>
2023-11-17 02:36:10 +0000

    【新增】 统一异常

--- core/src/main/java/com/qing/owl/core/domain/exception/BaseException.java
+++ core/src/main/java/com/qing/owl/core/domain/exception/BaseException.java
@@ -1,5 +1,18 @@
 package com.qing.owl.core.domain.exception;
 
+import lombok.Getter;
+
+@Getter
 public abstract class BaseException extends RuntimeException{
     private ErrorCode errorCode;
+
+    public BaseException(ErrorCode errorCode){
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public BaseException(ErrorCode errorCode,Throwable cause){
+        super(errorCode.getMessage(),cause);
+        this.errorCode = errorCode;
+    }
 }
--- core/src/main/java/com/qing/owl/core/domain/exception/SystemException.java
+++ core/src/main/java/com/qing/owl/core/domain/exception/SystemException.java
@@ -1,5 +1,9 @@
 package com.qing.owl.core.domain.exception;
 
 public class SystemException extends BaseException {
+
+    public SystemException(SystemErrorCode errorCode) {
+        super(errorCode);
+    }
     
 }

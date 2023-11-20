46c4d5ebd056380534755dcbdf821cbaa13a6a27
WaylonCoffee <33085998+WaylonCoffee@users.noreply.github.com>
2023-11-20 09:37:26 +0000

    【新增】 logback日志以及MDC链路

---
+++ core/src/main/java/com/qing/owl/core/infra/config/WebConfig.java
@@ -0,0 +1,26 @@
+package com.qing.owl.core.infra.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.qing.owl.core.infra.Interceptor.LogInterceptor;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    
+    @Bean
+    public LogInterceptor logInterceptor() {
+        return new LogInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor());
+    }
+
+    
+
+    
+}

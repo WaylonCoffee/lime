02f9e96cea145453f25f9c0d2bed0e118b803c89
WaylonCoffee <33085998+WaylonCoffee@users.noreply.github.com>
2023-11-20 10:14:03 +0000

    【新增】 1.logback 日志 2.MDC链路追踪

---
+++ core/src/main/java/com/qing/owl/core/infra/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.qing.owl.core.infra.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.qing.owl.core.infra.interceptor.LogInterceptor;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    
+    @Bean
+    public LogInterceptor logInterceptor() {
+        return new LogInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor());
+    }
+}
--- core/src/main/java/com/qing/owl/core/interfaces/controller/assembler/AboutController.java
+++ core/src/main/java/com/qing/owl/core/interfaces/controller/assembler/AboutController.java
@@ -4,12 +4,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
 @RequestMapping("about")
+@Slf4j
 public class AboutController {
     
     @GetMapping("health")
     public String health(){
+      log.error("null32423");
         return "hello";
     }
 }
